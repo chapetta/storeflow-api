458f1b4519a8df8c8c845d4e74470cb20549fe6c
chapetta <cha.petta@hotmail.com>
2025-11-14 17:49:03 -0300

    remove dist folder from version control

--- dist/modules/users/users.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usersRoutes = void 0;
-const express_1 = require("express");
-const users_controller_1 = require("./users.controller");
-const router = (0, express_1.Router)();
-exports.usersRoutes = router;
-router.post("/", users_controller_1.usersController.create);
-router.get("/", users_controller_1.usersController.list);
